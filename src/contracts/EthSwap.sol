506f196a95e074ea792c5240030b5f0bff0180bc
mister-bee <thebeech@yahoo.com>
2021-04-05 16:35:03 -0700

    build test suite and add helpers

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,16 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol"
+
 contract EthSwap {
+  // name is a state variable
     string public name = "EthSwap Instant Exchange";
-}
+    Token public token;
+
+    // tell this contract where...
+    // we need the code and the addresss
+    constructor(Token _token) public {
+      token = _token
+
+
+}}
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -7,14 +7,39 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
+function tokens(n) {
+  return web3.utils.toWei(n, 'ether')
+}
+
 contract('EthSwap', (accounts) => {
+  let token, ethSwap
+
+  before(async () => {
+    token = await Token.new()
+    ethSwap = await EthSwap.new()
+    await token.transfer(ethSwap.address, tokens('1000000'))
+  })
+
+  describe('Token  deployment', async () => {
+    it('contract has a name', async () => {
+      const name = await token.name()
+      assert.equal(name, 'DApp Token')
+    })
+  })
 
   describe('EthSwap deployment', async () => {
     it('contract has a name', async () => {
-      let ethSwap = await EthSwap.new()
       const name = await ethSwap.name()
       assert.equal(name, 'EthSwap Instant Exchange')
     })
   })
+
+  it('contract has tokens', async () => {
+    let balance = await token.balanceOf(ethSwap.address)
+    assert.equal(balance.toString(), tokens('1000000'))
+  })
+
+
+
 
 })
