0a5f1942c20d6a300386a64078c8ae2c660f268f
mister-bee <thebeech@yahoo.com>
2021-04-12 21:55:21 -0700

    add buytokens fn to Ethswap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,13 +4,14 @@ const EthSwap = artifacts.require("EthSwap");
 module.exports = async function (deployer) {
   // Deploy Token
   await deployer.deploy(Token);
-  const token = await Token.deployed()
+  const token = await Token.deployed();
 
   // Deploy EthSwap
-  await deployer.deploy(EthSwap);
-  const ethSwap = await EthSwap.deployed()
+  await deployer.deploy(EthSwap, token.address);
+  const ethSwap = await EthSwap.deployed();
 
   // Trans all tokens to EthSwap
-  await token.transfer(ethSwap.address, "1000000000")
+  await token.transfer(ethSwap.address, "1000000000");
 };
 
+// node v.9.10.0
--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,16 +1,28 @@
 pragma solidity ^0.5.0;
 
-import "./Token.sol"
+import "./Token.sol";
 
 contract EthSwap {
-  // name is a state variable
+    // name is a state variable, stored on blockchain
     string public name = "EthSwap Instant Exchange";
     Token public token;
+    uint256 public rate = 100;
 
     // tell this contract where...
     // we need the code and the addresss
     constructor(Token _token) public {
-      token = _token
+        token = _token;
+    }
 
-
-}}
+    // msg is global variable inside of sol
+    // person calling function is sending ETH to buy tokens
+    // payable = key word to send eth
+    function buytokens() public payable {
+        // Calculate the number of tokens to buy
+        // Redemption rate = # of tokens they receive for 1 ETH
+        // Amount of EHT * redemption rate
+        uint256 tokenAmount = msg.value * rate; // tell us how much ether sent when fn called
+        token.transfer(msg.sender, tokenAmount);
+    }
+}
+// TUES MAKEUP - 1:09:31
